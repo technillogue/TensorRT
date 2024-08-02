7086f3da4c9d443d8b6cebfdd12cee306f0a2528
lanluo-nvidia <lanl@nvidia.com>
2024-08-01 18:53:20 -0700

    add quantization support for int8

--- py/torch_tensorrt/dynamo/conversion/impl/quantize.py
+++ py/torch_tensorrt/dynamo/conversion/impl/quantize.py
@@ -18,10 +18,38 @@ def quantize_fp8(
     input_tensor: TRTTensor,
     scale: np.ndarray,
 ) -> TRTTensor:
+    return quantize(ctx, target, source_ir, name, "fp8", input_tensor, scale)
+
+
+def quantize_int8(
+    ctx: ConversionContext,
+    target: Target,
+    source_ir: Optional[SourceIR],
+    name: str,
+    input_tensor: TRTTensor,
+    scale: np.ndarray,
+) -> TRTTensor:
+    return quantize(ctx, target, source_ir, name, "int8", input_tensor, scale)
+
+
+def quantize(
+    ctx: ConversionContext,
+    target: Target,
+    source_ir: Optional[SourceIR],
+    name: str,
+    quantize_type: str,
+    input_tensor: TRTTensor,
+    scale: np.ndarray,
+) -> TRTTensor:
     """
     Adds quantize and dequantize ops (QDQ) which quantize to INT8 or FP8 based
     on the output_type set and dequantizes them back.
     """
+    if quantize_type not in ["fp8", "int8"]:
+        raise ValueError(
+            f"quantize converter received an type of {type}. Supported quantize type: fp8, int8"
+        )
+
     if (isinstance(input_tensor, TRTTensor)) and not (
         input_tensor.dtype == trt.float32 or input_tensor.dtype == trt.float16
     ):
@@ -32,14 +60,21 @@ def quantize_fp8(
     scale = get_trt_tensor(ctx, scale, name + "_scale")
     # Add Q node
     quantize_layer = ctx.net.add_quantize(input_tensor, scale)
-    quantize_layer.set_output_type(0, trt.DataType.FP8)
+    if quantize_type == "fp8":
+        quantize_layer.set_output_type(0, trt.DataType.FP8)
+    elif quantize_type == "int8":
+        quantize_layer.set_output_type(0, trt.DataType.INT8)
+
     set_layer_name(quantize_layer, target, name + "_quantize", source_ir)
     q_output = quantize_layer.get_output(0)
     # Add DQ node
     dequantize_layer = ctx.net.add_dequantize(q_output, scale)
     set_layer_name(dequantize_layer, target, name + "_dequantize", source_ir)
-    # Set DQ layer precision to FP8
-    dequantize_layer.precision = trt.DataType.FP8
+    if quantize_type == "fp8":
+        # Set DQ layer precision to FP8
+        dequantize_layer.precision = trt.DataType.FP8
+    elif quantize_type == "int8":
+        dequantize_layer.precision = trt.DataType.INT8
     dq_output = dequantize_layer.get_output(0)
 
     return dq_output
